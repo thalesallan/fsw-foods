c8dc4f68f4b27674d4ccecb2b24f4163eb38b7c7
Thales Allan <trabalhecomthales@gmail.com>
2024-04-30 00:20:27 -0300

    chore: add eslint break config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-<div className="bg-red-500 p-5">
-  <h1>Hello word</h1>
-</div>
+    <div className="bg-red-500 p-5">
+      <h1>Hello word</h1>
+    </div>
   );
 }
