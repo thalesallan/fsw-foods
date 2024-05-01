146f2dc52fd1efc49d1343b4475d18534e46b02f
Thales Allan <trabalhecomthales@gmail.com>
2024-05-01 08:26:20 -0300

    feat: promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -13,6 +14,18 @@ export default function Home() {
 
       <div className="p-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-2">
+        <Image
+          src="/promo-banner.png"
+          alt="Banner Desconto de até 30% em Pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={50}
+        />
       </div>
     </>
   );
